09d9274ab421179b85af241d7c11e4e1bb88583a
Asad Quraishi <asad@cingularity.ca>
2017-01-30 16:59:03 -0500

    Change to drive.py that got it driving (added dim to image)

--- drive.py
+++ drive.py
@@ -1,7 +1,6 @@
 import argparse
 import base64
 import json
-
 import numpy as np
 import socketio
 import eventlet
@@ -20,11 +19,19 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
-
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
+
+# AQ - Create a normalizer for both images and angles
+def normalizer(array, min_max=(0,1), feature_range=(0, 1)):
+    x_min = feature_range[0]
+    x_max = feature_range[1]
+    a = min_max[0]
+    b = min_max[1]
+    norm_features = (array-x_min)*(b-a)/(x_max - x_min) + a
+    return norm_features
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -38,12 +45,21 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
-    transformed_image_array = imresize(image, (100, 200, 3))[16:, :, :][:66, :, :]
-    #transformed_image_array = image_array[None, :, :, :]
+    print("Image dims are",image_array.shape)
+    # AQ add following to resize image from camera
+    transformed_image_array = imresize(image_array, (100, 200, 3))[16:, :, :][:66, :, :]
+    transformed_image_array = transformed_image_array[None, :, :, :]
+    print("Resized image dims are", transformed_image_array.shape)
+    print("Image RGB settings are", transformed_image_array[0,0,0])
+
+    transformed_image_array = normalizer(transformed_image_array, min_max=(0, 1), feature_range=(0, 255))
+    print("New RGB settings are", transformed_image_array[0, 0, 0])
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    print("Steering angle", steering_angle)
+    steering_angle = normalizer(steering_angle, min_max=(-1.0, 1.0), feature_range=(-0.5, 0.5))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 5.0
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
@@ -74,7 +90,6 @@ if __name__ == '__main__':
         #
         # instead.
         model = model_from_json(jfile.read())
-
 
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
