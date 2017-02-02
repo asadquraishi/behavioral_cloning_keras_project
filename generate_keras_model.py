1c3b571ce3fca641b653233ac7c544046b8b98ba
Asad Quraishi <asad@cingularity.ca>
2017-02-02 09:26:10 -0500

    Added try/accept so I can load the model to train for additional images

--- generate_keras_model.py
+++ generate_keras_model.py
@@ -19,59 +19,72 @@ data_file = open('image_test_data.pkl', 'rb')
 X_test, y_test = pickle.load(data_file)
 data_file.close()
 
-#Train NN
-# 1st Layer - Convnet
-model = Sequential()
-model.add(Convolution2D(24, 5, 5,border_mode='valid',input_shape=X_train.shape[1:],subsample=(2, 2)))
-#model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+# Load the model if we want to train it on additional data
+try:
+    # Load the model if we want to train it on additional data
+    with open('model.json', 'r') as jfile:
+        model = model_from_json(jfile.read())
 
-# 2nd Layer - Convnet
-model.add(Convolution2D(36, 5, 5,border_mode='valid',subsample=(2, 2)))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    model.load_weights('model.h5')
 
-# 3rd Layer - Convnet
-model.add(Convolution2D(48, 5, 5,border_mode='valid',subsample=(2, 2)))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+    print("imported existing model")
 
-# 4th Layer - Convnet
-model.add(Convolution2D(64, 3, 3,border_mode='valid'))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+except:
+    # Else build the model
+    # 1st Layer - Convnet
+    model = Sequential()
+    model.add(Convolution2D(24, 5, 5,border_mode='valid',input_shape=X_train.shape[1:],subsample=(2, 2)))
+    #model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
 
-# 5th Layer - Convnet
-model.add(Convolution2D(64, 3, 3,border_mode='valid'))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+    # 2nd Layer - Convnet
+    model.add(Convolution2D(36, 5, 5,border_mode='valid',subsample=(2, 2)))
+    model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
 
-# Flatten
-model.add(Flatten())
+    # 3rd Layer - Convnet
+    model.add(Convolution2D(48, 5, 5,border_mode='valid',subsample=(2, 2)))
+    model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
 
-# Fully connected layer
-model.add(Dense(100))
-#model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+    # 4th Layer - Convnet
+    model.add(Convolution2D(64, 3, 3,border_mode='valid'))
+    model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
 
-# Fully connected layer
-model.add(Dense(50))
-#model.add(Activation('relu'))
+    # 5th Layer - Convnet
+    model.add(Convolution2D(64, 3, 3,border_mode='valid'))
+    model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
 
-# Fully connected layer
-model.add(Dense(10))
-#model.add(Activation('relu'))
+    # Flatten
+    model.add(Flatten())
 
-# Output
-model.add(Dense(1))
+    # Fully connected layer
+    model.add(Dense(100))
+    #model.add(Dropout(0.5))
+    #model.add(Activation('relu'))
+
+    # Fully connected layer
+    model.add(Dense(50))
+    #model.add(Activation('relu'))
+
+    # Fully connected layer
+    model.add(Dense(10))
+    #model.add(Activation('relu'))
+
+    # Output
+    model.add(Dense(1))
+
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
 print(model.summary())
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 history = model.fit(X_train, y_train, batch_size=15, nb_epoch=10, validation_split=0.0, validation_data=(X_validation, y_validation),verbose=1)
-score = model.evaluate(X_test, y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+assess = model.evaluate(X_test, y_test, verbose=0)
+print('Accuracy:', score[0])
+print('Loss:', score[1])
 
 # Save the model
 json_model = model.to_json()
