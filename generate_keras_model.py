7952b0a14ba3110706054dc83c9f7f55e6311247
Asad Quraishi <asad@cingularity.ca>
2017-02-02 08:27:13 -0500

    remove last dropout

--- generate_keras_model.py
+++ generate_keras_model.py
@@ -43,7 +43,7 @@ model.add(Dropout(0.5))
 
 # 5th Layer - Convnet
 model.add(Convolution2D(64, 3, 3,border_mode='valid'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 #model.add(Activation('relu'))
 
 # Flatten
